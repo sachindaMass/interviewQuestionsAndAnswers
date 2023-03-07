47344e163cb27b6bb1742fb17917018209f7ce97
sachindaMass <sachinda46@gmail.com>
2023-03-07 19:03:07 +0530

    Rest Example unfinished

---
+++ src/main/java/com/example/interviewquestionsandanswers/restExample/Constants.java
@@ -0,0 +1,11 @@
+/*
+ * @created 07/03/2023 - 6:36 PM
+ * @project interviewQuestionsAndAnswers
+ * @author  Dell-PC
+ */
+package com.example.interviewquestionsandanswers.restExample;
+
+public class Constants {
+
+    static final String API_KEY = "";
+}
---
+++ src/main/java/com/example/interviewquestionsandanswers/restExample/RestExample.java
@@ -0,0 +1,20 @@
+/*
+ * @created 07/03/2023 - 6:31 PM
+ * @project interviewQuestionsAndAnswers
+ * @author  Dell-PC
+ */
+package com.example.interviewquestionsandanswers.restExample;
+
+import org.aspectj.apache.bcel.classfile.Constant;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+
+public class RestExample {
+
+//    HttpRequest httpRequest = HttpRequest.newBuilder() //TODO
+//            .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+//            .header("Authorization", Constants.API_KEY)
+//            .POST(HttpRequest.BodyPublisher.class);
+
+}
